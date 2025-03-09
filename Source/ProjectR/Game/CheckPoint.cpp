59af55211810a63e6f7ccfb6dfb027f44a5f5426
GoldFrosch <goldfrosch@daum.net>
2025-03-10 00:17:26 +0900

    feat: 체크포인트 분기 처리에 대한 대응 완료

--- Source/ProjectR/Game/CheckPoint.cpp
+++ Source/ProjectR/Game/CheckPoint.cpp
@@ -46,34 +46,32 @@ void ACheckPoint::SaveCheckPoint(const AActor* CheckKart)
 	
 	GetCheckPointPinInfo(CheckPointPin, CurrentPlayerPin);
 
+	const uint16 CurrentPlayerPinMainNum = CurrentPlayerPin[0];
+	const uint16 CurrentCheckPointPinMainNum = CurrentCheckPointPinNumArray[0];
+	
 	// 두개의 길이가 같은 경우는 계속 숫자들을 순회하면서 마지막 번호가 달라졌는지 확인해야 한다.
 	// 갯수가 같은 경우는 무조건 분기점 추가가 아닌 다음 핀 포인트로 이동했음을 의미한다.
-	// 여기서 4-2 -> 5-1 의 케이스도 있을 수 있으나, 해당 케이스는 고려하지 않는다.
-	// 반드시 분기점 이전에 체크포인트를 설정하는 것을 가정하고 작업한다.
+	// 핀 포인트 분기 이동 방식은 4-2 -> 5-2 같은 항상 체크포인트는 메인 핀 포인트를 기점으로
+	// 움직이게 끔 처리된다.
 	// TODO: 해당 부분은 이후 툴 제작에도 영향을 줘야 할 필요가 있다.
-	if (CurrentPlayerPin.Num() == CurrentCheckPointPinNumArray.Num())
-	{
-		// 단순하게 다음 핀으로 넘어가는 과정이라면
-		// 아래와 같이 현재 체크포인트의 핀으로 설정해주면 된다.
-		const bool IsNextPinToMove = CurrentPlayerPin[CurrentPlayerPin.Num() - 1] + 1 == CurrentCheckPointPinNumArray[CurrentCheckPointPinNumArray.Num() - 1];
+	const bool IsNextPinToMove = CurrentPlayerPinMainNum + 1 == CurrentCheckPointPinMainNum;
 		
-		if (IsNextPinToMove)
-		{
-			PS->SetCheckPoint(CurrentCheckPoint);
-			UE_LOG(LogTemp, Display, TEXT("현재 정보: %d Laps, %s CheckPoint"), PS->GetCurrentLap(), *PS->GetCurrentKartCheckPoint());
-			return;
-		}
+	if (IsNextPinToMove)
+	{
+		PS->SetCheckPoint(CurrentCheckPoint);
+		UE_LOG(LogTemp, Display, TEXT("현재 정보: %d Laps, %s CheckPoint"), PS->GetCurrentLap(), *PS->GetCurrentKartCheckPoint());
+		return;
+	}
 
-		// 위에 과정 즉 다음 핀으로 넘어가는 것이 아닌 바퀴 수가 넘어가는 조건 즉
-		// 모든 체크포인트를 순회했으며, 다음 핀이 0인 경우는 무조건 한 바퀴를 돌았다고 가정한다.
-		const bool IsNextLapToMove = CurrentPlayerPin[0] == GetWorld()->GetGameState<ARaceGameState>()->GetMaxCheckPoint()
-									&& CurrentCheckPointPinNumArray[0] == 0;
+	// 위에 과정 즉 다음 핀으로 넘어가는 것이 아닌 바퀴 수가 넘어가는 조건 즉
+	// 모든 체크포인트를 순회했으며, 다음 핀이 0인 경우는 무조건 한 바퀴를 돌았다고 가정한다.
+	const bool IsNextLapToMove = CurrentPlayerPinMainNum == GetWorld()->GetGameState<ARaceGameState>()->GetMaxCheckPoint()
+								&& GetPinMainNumber() == 0;
 
-		if (IsNextLapToMove)
-		{
-			PS->GoNextLap();
-			UE_LOG(LogTemp, Display, TEXT("현재 정보: %d Laps, %s CheckPoint"), PS->GetCurrentLap(), *PS->GetCurrentKartCheckPoint());
-		}
+	if (IsNextLapToMove)
+	{
+		PS->GoNextLap();
+		UE_LOG(LogTemp, Display, TEXT("현재 정보: %d Laps, %s CheckPoint"), PS->GetCurrentLap(), *PS->GetCurrentKartCheckPoint());
 	}
 }
 
