5e83222d04513813449b7ef04f547ec9cbff4b6a
GoldFrosch <goldfrosch@daum.net>
2025-03-10 18:16:59 +0900

    feat: 카운트다운 UI 추가

---
+++ Source/ProjectR/KartGame/UIs/HUD/CountDownToStart.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CountDownToStart.generated.h"
+
+class UTextBlock;
+
+UCLASS()
+class PROJECTR_API UCountDownToStart : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	FWidgetAnimationDynamicEvent OnCountEndNotified;
+	
+protected:
+	virtual void NativePreConstruct() override;
+	
+	virtual void NativeOnInitialized() override;
+
+private:
+	UPROPERTY(BlueprintReadWrite, Category = "Options|UI", meta = (AllowPrivateAccess = true))
+	uint8 CountDownNum = 3;
+	
+	UPROPERTY(BlueprintReadWrite, Category = "Options|UI", meta = (AllowPrivateAccess = true))
+	FString StartText;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UTextBlock> CountDownText;
+	
+	UPROPERTY(Transient, meta = (BindWidgetAnim))
+	TObjectPtr<UWidgetAnimation> CountDownAnimation;
+
+	UFUNCTION()
+	void OnCountDownAnimationEnd();
+
+	void UpdateCountDownText();
+};
---
+++ Source/ProjectR/KartGame/UIs/HUD/MainUI.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MainUI.generated.h"
+
+UCLASS()
+class PROJECTR_API UMainUI : public UUserWidget
+{
+	GENERATED_BODY()
+};
