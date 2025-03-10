b2ad3736b803a84cc0bdd0b285dcb16988a31c72
Jangjinhyeok <zero9936@gmail.com>
2025-03-10 15:40:02 +0900

    게임 인스턴스 추가

---
+++ Source/ProjectR/Games/KartGameInstance.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "KartGameInstance.generated.h"
+
+/**
+ * 
+ */
+USTRUCT(BlueprintType)
+struct FItemTable : public FTableRowBase
+{
+	GENERATED_USTRUCT_BODY()
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Item)
+	FName ItemName;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Item)
+	int32 ItemWeight;	
+};
+
+UCLASS()
+class PROJECTR_API UKartGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+};
