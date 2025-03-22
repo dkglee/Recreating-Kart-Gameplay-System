d5f6f12e0f354cad63d37f61bcf99d8a4c64f227
GoldFrosch <goldfrosch@daum.net>
2025-03-22 12:10:22 +0900

    feat: 로비 전용 Stack 구조 위젯 기능 구현

--- Source/ProjectR/KartGame/Games/Modes/Lobby/LobbyPlayerController.cpp
+++ Source/ProjectR/KartGame/Games/Modes/Lobby/LobbyPlayerController.cpp
@@ -1,12 +1,48 @@
 ﻿#include "LobbyPlayerController.h"
 
 #include "Blueprint/UserWidget.h"
+#include "EnhancedInputComponent.h"
 #include "KartGame/UIs/Lobby/MainLobbyUI.h"
 
 void ALobbyPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
 
+	SetShowMouseCursor(true);
 	MainLobbyUI = CreateWidget<UMainLobbyUI>(this, MainLobbyUIClass);
 	MainLobbyUI->AddToViewport(0);
+}
+
+void ALobbyPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(InputComponent);
+	if (!EnhancedInputComponent)
+	{
+		return;
+	}
+
+	EnhancedInputComponent->BindAction(IA_ExitWidget, ETriggerEvent::Started, this,
+		&ThisClass::PopWidgetStack);
+}
+
+void ALobbyPlayerController::PushWidgetStack(UUserWidget* NewWidget)
+{
+	WidgetStack.Push(NewWidget);
+	
+	WidgetStack.Last()->SetVisibility(ESlateVisibility::Hidden);
+	NewWidget->SetVisibility(ESlateVisibility::Collapsed);
+}
+
+void ALobbyPlayerController::PopWidgetStack()
+{
+	if (WidgetStack.Num() == 0)
+	{
+		// TODO: ESC 화면 노출
+		return;
+	}
+
+	WidgetStack.Last()->SetVisibility(ESlateVisibility::Hidden);
+	WidgetStack.Pop();
 }
--- Source/ProjectR/KartGame/Games/Modes/Lobby/LobbyPlayerController.h
+++ Source/ProjectR/KartGame/Games/Modes/Lobby/LobbyPlayerController.h
@@ -1,30 +1,42 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
-#pragma once
+﻿#pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
 #include "LobbyPlayerController.generated.h"
 
+class UInputAction;
+class UInputMappingContext;
 class UMainLobbyUI;
-/**
- * 
- */
+
 UCLASS()
 class PROJECTR_API ALobbyPlayerController : public APlayerController
 {
 	GENERATED_BODY()
-
-public:
-	void ActiveLobbyUI();
 	
 protected:
 	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
 	
 private:
-	UPROPERTY(EditDefaultsOnly, Category = "Options|UI", meta = (AllowPrivateAccess = true))
-	TSubclassOf<UMainLobbyUI> MainLobbyUIClass;
-	
 	UPROPERTY()
 	TObjectPtr<UMainLobbyUI> MainLobbyUI;
+
+	// TODO: 모듈화
+	TArray<TObjectPtr<UUserWidget>> WidgetStack;
+
+	void PushWidgetStack(UUserWidget* NewWidget);
+	void PopWidgetStack();
+
+#pragma region Input
+	UPROPERTY(EditDefaultsOnly, Category = "Options|Input", meta = (AllowPrivateAccess = true))
+	TObjectPtr<UInputMappingContext> IMC_Lobby;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Options|Input", meta = (AllowPrivateAccess = true))
+	TObjectPtr<UInputAction> IA_ExitWidget;
+#pragma endregion
+
+#pragma region WidgetFactory
+	UPROPERTY(EditDefaultsOnly, Category = "Options|UI", meta = (AllowPrivateAccess = true))
+	TSubclassOf<UMainLobbyUI> MainLobbyUIClass;
+#pragma endregion 
 };
