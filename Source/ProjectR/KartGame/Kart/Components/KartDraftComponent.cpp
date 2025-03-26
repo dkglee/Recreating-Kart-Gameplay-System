2d11324e77b32db4c17ac274c6a915399863765d
Jangjinhyeok <zero9936@gmail.com>
2025-03-26 16:06:14 +0900

    드래프트 기능 완성

--- Source/ProjectR/KartGame/Kart/Components/KartDraftComponent.cpp
+++ Source/ProjectR/KartGame/Kart/Components/KartDraftComponent.cpp
@@ -3,6 +3,14 @@
 
 #include "KartDraftComponent.h"
 
+#include "FastLogger.h"
+#include "Kart.h"
+#include "KartAccelerationComponent.h"
+#include "KartNetworkSyncComponent.h"
+#include "KartSuspensionComponent.h"
+#include "Components/BoxComponent.h"
+#include "Net/UnrealNetwork.h"
+
 
 // Sets default values for this component's properties
 UKartDraftComponent::UKartDraftComponent()
@@ -10,22 +18,166 @@ UKartDraftComponent::UKartDraftComponent()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
+	bWantsInitializeComponent = true;
+	SetIsReplicatedByDefault(true);
 
-	// ...
 }
 
 void UKartDraftComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
 	
+}
+
+void UKartDraftComponent::InitializeComponent()
+{
+	Super::InitializeComponent();
+
+	Kart = Cast<AKart>(GetOwner());
 }
 
 void UKartDraftComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	if (Kart->IsLocallyControlled() == false) return;
+	
+	if (bDraftStart == false)
+	{
+		FVector start = Kart->GetRootComponent()->GetComponentLocation();
+		FVector end = Kart->GetRootComponent()->GetComponentLocation() + Kart->GetRootComponent()->GetForwardVector() * DraftDistance;
+		Server_FindTarget(start, end, TraceBoxHalfSize);
+	}
+	else
+	{
+		AddDraftForce();
+	}
 }
 
+void UKartDraftComponent::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(UKartDraftComponent, bDraftStart);
+}
+
+void UKartDraftComponent::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	GetWorld()->GetTimerManager().ClearTimer(DraftTimerHandle);
+	Super::EndPlay(EndPlayReason);
+}
+
+void UKartDraftComponent::DrawTraceLineBox(FVector start, FVector end, FVector boxHalfSize, FColor boxColor)
+{
+	// 서버만 보이도록 설정
+	if (Kart->HasAuthority() == false) return;
+	
+	int NumSteps = 10;
+	for (int i = 0; i <= NumSteps; i++)
+	{
+		float Alpha = (float)i / NumSteps;
+		FVector DebugLocation = FMath::Lerp(start, end, Alpha);
+	
+		DrawDebugBox(GetWorld(), DebugLocation, boxHalfSize, Kart->GetRootComponent()->GetComponentQuat(), boxColor, false, 0.1f);
+	}
+}
+
+void UKartDraftComponent::Server_FindTarget_Implementation(FVector start, FVector end, FVector boxHalfSize)
+{
+	FCollisionQueryParams Params;
+	Params.AddIgnoredActor(GetOwner());
+
+	// 앞에 있는 카트 중에 가장 가까운 카트를 찾는 로직
+	float ClosestDistance = TNumericLimits<float>::Max();
+
+	TArray<FHitResult> InitialHitResults;
+	FVector InitialBoxSize = boxHalfSize;
+
+	bool bInitialHit = GetWorld()->SweepMultiByChannel(
+		InitialHitResults,
+		start,
+		end,
+		Kart->GetRootComponent()->GetComponentQuat(),
+		ECC_Pawn,
+		FCollisionShape::MakeBox(InitialBoxSize),
+		Params);
+
+	if (bInitialHit)
+	{
+		for (const FHitResult& Hit : InitialHitResults)
+		{
+			AKart* PotentialTarget = Cast<AKart>(Hit.GetActor());
+			if (PotentialTarget && PotentialTarget != FinalTarget)
+			{
+				float Distance = FVector::Distance(start, Hit.ImpactPoint);
+				if (Distance < ClosestDistance)
+				{
+					ClosestDistance = Distance;
+					FinalTarget = PotentialTarget;
+				}
+			}
+			else if (FinalTarget == PotentialTarget)
+			{
+				continue;
+			}
+			else
+			{
+				FinalTarget = nullptr;
+			}
+		}
+	}
+
+	FColor boxColor = FColor::Green;
+	if (FinalTarget != nullptr)
+	{
+		boxColor = FColor::Red;
+		CheckTraceTime();
+	}
+
+	DrawTraceLineBox(start, end, boxHalfSize, boxColor);
+}
+
+void UKartDraftComponent::CheckTraceTime()
+{
+	if (Kart->HasAuthority() == false) return;
+
+	if (Kart->GetNetworkSyncComponent()->GetKartInfo().Velocity.Size() < 100.f)
+	{
+		FFastLogger::LogConsole(TEXT("카트 속도가 너무 낮아요"));
+		return;
+	}
+	
+
+	ElapsedTime += GetWorld()->GetDeltaSeconds();
+
+	if (ElapsedTime >= DraftStartTime && bDraftStart == false)
+	{
+		FFastLogger::LogConsole(TEXT("DRAFT!"));
+		bDraftStart = true;
+		ElapsedTime = 0.f;
+		GetWorld()->GetTimerManager().ClearTimer(DraftTimerHandle);
+		TWeakObjectPtr<UKartDraftComponent> WeakThis = this;
+		GetWorld()->GetTimerManager().SetTimer(DraftTimerHandle, [WeakThis]()
+		{
+			if (WeakThis.IsValid())
+			{
+				WeakThis->bDraftStart = false;
+			}
+		}, DraftDuration, false);
+	}
+}
+
+void UKartDraftComponent::AddDraftForce()
+{
+	auto* KartBody = Cast<UBoxComponent>(Kart->GetRootComponent());
+	auto* AccelerationComponent = Cast<UKartAccelerationComponent>(Kart->GetAccelerationComponent());
+	
+	FVector force = KartBody->GetForwardVector() * KartBody->GetMass() * DraftForce;
+    
+	for (int32 i = 0; i < AccelerationComponent->GetWheels().Num(); i++)
+	{
+		FVector location = AccelerationComponent->GetWheels()[i]->GetComponentLocation();
+		KartBody->AddForceAtLocation(force, location);
+	}
+}
--- Source/ProjectR/KartGame/Kart/Components/KartDraftComponent.h
+++ Source/ProjectR/KartGame/Kart/Components/KartDraftComponent.h
@@ -19,7 +19,51 @@ public:
 protected:
 	virtual void BeginPlay() override;
 
+	virtual void InitializeComponent() override;
+
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
 	
-public:
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+private:
+	void DrawTraceLineBox(FVector start, FVector end, FVector boxHalfSize, FColor boxColor);
+
+	UFUNCTION(Server, Reliable)
+	void Server_FindTarget(FVector start, FVector end, FVector boxHalfSize);
+
+	void CheckTraceTime();
+
+	void AddDraftForce();
+	
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Draft", meta = (AllowPrivateAccess = "true"))
+	class AKart* Kart = nullptr;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Draft", meta = (AllowPrivateAccess = "true"))
+	class AKart* FinalTarget = nullptr;
+	
+	float ElapsedTime = 0.f;
+
+	UPROPERTY(Replicated, EditAnywhere, BlueprintReadWrite, Category = "Draft", meta = (AllowPrivateAccess = "true"))
+	bool bDraftStart = false;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Draft", meta = (AllowPrivateAccess = "true"))
+	float DraftStartTime = 3.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Draft", meta = (AllowPrivateAccess = "true"))
+	float DraftDuration = 1.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Draft", meta = (AllowPrivateAccess = "true"))
+	float DraftDistance = 1000.f;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Draft", meta = (AllowPrivateAccess = "true"))
+	FVector TraceBoxHalfSize = FVector(100.f);
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Draft", meta = (AllowPrivateAccess = "true"))
+	float DraftForce = 150.f;
+
+	FTimerHandle DraftTimerHandle;
+	
 };
