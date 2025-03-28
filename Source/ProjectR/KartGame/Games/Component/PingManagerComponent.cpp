6c20d1959d2efc30c9fcb3359a4d7c88e39786de
GoldFrosch <goldfrosch@daum.net>
2025-03-28 19:35:26 +0900

    feat: PingManagerComponent 추가

---
+++ Source/ProjectR/KartGame/Games/Component/PingManagerComponent.cpp
@@ -0,0 +1,64 @@
+﻿#include "PingManagerComponent.h"
+
+#include "FastLogger.h"
+#include "KartGame/Games/KartGameInstance.h"
+#include "Net/UnrealNetwork.h"
+
+UPingManagerComponent::UPingManagerComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UPingManagerComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void UPingManagerComponent::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME(UPingManagerComponent, IsPingReceived);
+}
+
+void UPingManagerComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                          FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	if (GetOwner()->HasAuthority())
+	{
+		PingToServer();
+	} else
+	{
+		Server_PingToServer();
+	}
+}
+
+void UPingManagerComponent::PingToServer()
+{
+	if (IsPingReceived)
+	{
+		return;
+	}
+
+	FFastLogger::LogScreen(FColor::Emerald, TEXT("핑 성공 하다: %s"), *GetName());
+	
+	// 서버의 핑 카운트만 증가
+	GetWorld()->GetGameInstance<UKartGameInstance>()->SetPingCount(
+		GetWorld()->GetGameInstance<UKartGameInstance>()->GetPingCount() + 1);
+	
+	IsPingReceived = true;
+	PrimaryComponentTick.bCanEverTick = false;
+
+	if (GetWorld()->GetGameInstance<UKartGameInstance>()->GetPingCount()
+		== GetWorld()->GetGameInstance<UKartGameInstance>()->GetInGamePlayerCount())
+	{
+		FFastLogger::LogScreen(FColor::Emerald, TEXT("게임 시작하다: %s"), *GetName());
+		OnAllPingAccessNotified.Broadcast();
+	}
+}
+
+void UPingManagerComponent::Server_PingToServer_Implementation()
+{
+	PingToServer();
+}
---
+++ Source/ProjectR/KartGame/Games/Component/PingManagerComponent.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "PingManagerComponent.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnAllPingAccessNotified);
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class PROJECTR_API UPingManagerComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UPingManagerComponent();
+
+	FOnAllPingAccessNotified OnAllPingAccessNotified;
+	
+protected:
+	virtual void BeginPlay() override;
+
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+	
+	virtual void GetLifetimeReplicatedProps(
+		TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
+
+private:
+	UPROPERTY(Replicated)
+	bool IsPingReceived = false;
+
+	void PingToServer();
+	
+	UFUNCTION(Server, Reliable)
+	void Server_PingToServer();
+};
