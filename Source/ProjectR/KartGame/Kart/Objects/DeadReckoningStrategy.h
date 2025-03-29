1c120a95ce97063f465f3c5e574923dde8305e45
dkglee <ideade9@gmail.com>
2025-03-29 19:54:30 +0900

    Feat: DeadReckoning Setup

---
+++ Source/ProjectR/KartGame/Kart/Objects/DeadReckoningStrategy.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NetworkInterpolationStrategy.h"
+#include "DeadReckoningStrategy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTR_API UDeadReckoningStrategy : public UNetworkInterpolationStrategy
+{
+	GENERATED_BODY()
+};
---
+++ Source/ProjectR/KartGame/Kart/Objects/NetworkInterpolationStrategy.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "NetworkInterpolationStrategy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTR_API UNetworkInterpolationStrategy : public UObject
+{
+	GENERATED_BODY()
+};
